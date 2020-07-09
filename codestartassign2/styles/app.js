fbf623e37ad941494ed393953494f833a6aaee56
murali2u4 <67507011+murali2u4@users.noreply.github.com>
2020-07-09 17:56:43 +0530

    Update app.js

--- codestartassign2/styles/app.js
+++ codestartassign2/styles/app.js
@@ -1,40 +1,53 @@
 (function() {
-    'use strict';
-    angular.module('LunchCheck', [])
-        .controller('LunchCheckController', LunchCheckController);
-    LunchCheckController.$inject = ['$scope'];
+  'use strict';
+  angular
+    .module('ShoppingListCheckOff', [])
+    .controller('ToBuyShoppingController', ToBuyShoppingController)
+    .controller('AlreadyBoughtShoppingController', AlreadyBoughtShoppingController)
+    .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
-    function LunchCheckController($scope) {
-        $scope.lunchList = "";
-        $scope.message = "";
-        $scope.helpMessage = "";
+  ToBuyShoppingController.$inject = ['ShoppingListCheckOffService'];
+  function ToBuyShoppingController(ShoppingListCheckOffService) {
+    var controller = this;
 
-        function setAlertStyle(color) {
-            $scope.inputStyle = {
-                'border-color': color
-            };
-            $scope.mesageStyle = {
-                'color': color
-            };
-        }
-        $scope.checkIfTooMuch = function() {
-            var lunchList = $scope.lunchList;
-            if (lunchList.length > 0) {
-                var count = 0;
-                var lunchArr = lunchList.split(",");
-                var lunchArrSize = lunchArr.length;
-                for (var i = 0; i < lunchArrSize; i++) {
-                    if (lunchArr[i].trim().length > 0) {
-                        count++;
-                    }
-                }
-                setAlertStyle('#398439');
-                $scope.message = count > 3 ? "Too much!" : "Enjoy!";
-                $scope.helpMessage = count < lunchArrSize ? "I don't consider empty item!" : "";
-            } else {
-                setAlertStyle('#B5555A');
-                $scope.message = "Please enter data first";
-            }
-        };
+    controller.toBuyItems = ShoppingListCheckOffService.getToBuyItems();
+
+    controller.doBuy = function(index) {
+      ShoppingListCheckOffService.doBuy(index);
     }
-})();
+  }
+
+  AlreadyBoughtShoppingController.$inject = ['ShoppingListCheckOffService'];
+  function AlreadyBoughtShoppingController(ShoppingListCheckOffService) {
+    var controller = this;
+
+    controller.boughtItems = ShoppingListCheckOffService.getBoughtItems();
+
+  }
+
+  function ShoppingListCheckOffService() {
+    var service = this;
+
+    var toBuyItems = [{ name: "cookies", quantity: 10 },
+      { name: "milk", quantity: 10 },
+      { name: "tea", quantity: 10 },
+      { name: "coffee", quantity: 10 },
+      { name: "sugar", quantity: 10 }]
+    var boughtItems = [];
+
+    this.doBuy = function (index) {
+      boughtItems.push(toBuyItems[index]);
+      toBuyItems.splice(index, 1);
+    }
+
+    this.getToBuyItems = function() {
+      return toBuyItems;
+    }
+
+    this.getBoughtItems = function() {
+      return boughtItems;
+    }
+  }
+
+}
+)();
